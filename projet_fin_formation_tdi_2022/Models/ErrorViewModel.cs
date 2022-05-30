73243df0e81b7629d26729180cab4991c4391c61
mouhsin007 <elqadiri.smp@gmail.com>
2022-05-30 19:11:10 +0200

    créer le projet

---
+++ projet_fin_formation_tdi_2022/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace projet_fin_formation_tdi_2022.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
